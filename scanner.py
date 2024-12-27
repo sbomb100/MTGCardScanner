1cfffbf77c96156d5fa9b45d6946c343aa402e23
Spencer Bone <82596034+sbomb100@users.noreply.github.com>
2024-12-27 16:40:26 -0500

    wrapped card is not working as intended

--- scanner.py
+++ scanner.py
@@ -1,12 +1,20 @@
-import cv2
-import numpy as np
+#used for images
 import time
+import cv2
+#used for matrix and perspective transformations
+import numpy as np
+#used to read the card texts
+import pytesseract
+
+import re
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 def preprocess_image(img):
-    
-    blurred = cv2.GaussianBlur(img, (5, 5), 0)
-    gray = cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY)
-
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    #kernel = np.ones((5, 5), np.uint8)
+    #imgDil = cv2.dilate(blurred, kernel, iterations=1)
+    #sharpened = cv2.filter2D(imgDil, -1, kernel)
     # Detect edges using Canny
     #why 50 vs 150, poerhaps use parameters?
     #threshold1 = cv2.getTrackbarPos("Threshold1", "Parameters")
@@ -14,7 +22,7 @@ def preprocess_image(img):
 
     #uses gradient detection to try to identify edges on gradscale image
     edges = cv2.Canny(blurred, 50, 150)
-
+   
     #makes list of shapes in the image in a hierarchy  
     #cv2.RETR_EXTERNAL - outermost shapes
     #cv2.CHAIN_APPROX_SIMPLE - remove redundant straight lines
@@ -38,28 +46,57 @@ def preprocess_image(img):
     #if 0.5 < aspect_ratio < 2:  # Roughly rectangular
     #cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-   
+def warp_card(img, contours):
+    x, y, w, h = cv2.boundingRect(contours)
+    #contours[i][j] where j is the contour and i is the point to be observed
+    pts = np.float32([contours[0][0], contours[1][0], contours[2][0], contours[3][0]])
+    target = np.float32([[0, 0], [w, 0], [w, h], [0, h]])
 
-cap = cv2.VideoCapture(0)
+    matrix = cv2.getPerspectiveTransform(pts, target)
+    return cv2.warpPerspective(img, matrix, (w, h))
+
+def extract_card(img):
+    #convert to greyscale for easier reading
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # PSM 6 is for single uniform blocks of text, rather then segmenting text
+    card_text = pytesseract.image_to_string(gray, config='--psm 6 --oem 3')  
+    return card_text
+
+def getPrice(img):
+    card_title = img[25:75, 34:400]
+    # cv2.imshow("Card Name",card_title)
+
+    card_name = re.sub('[^a-zA-Z0-9,+ ]', '', pytesseract.image_to_string(card_title))
+
+    print("Card name: ", card_name)
+
+    
+
+#open camera
+#cap = cv2.VideoCapture(0)
 
 while True:
-    success, img = cap.read()
-    if not success:
-        break
-
-    imgContour = img.copy()
-    imgConts = img.copy()
-   
-
-    card_contour = preprocess_image(img)
+    #frame capture
+    #success, img = cap.read()
+    #if not success:
+    #    break
+    
+    img = cv2.imread("C:\\card.jpg")
+    resized_image = cv2.resize(img, None, fx=0.3, fy=0.3)
+    #get the large edges
+    card_contour = preprocess_image(resized_image)
+        
 
     if card_contour is not None:
-        cv2.drawContours(img, [card_contour], -1, (0, 255, 0), 2)
+        wrapped_card = warp_card(resized_image, card_contour)
+       
+        cv2.imshow("Warped Card", wrapped_card)
+        cv2.drawContours(resized_image, [card_contour], -1, (0, 255, 0), 2)
 
-    cv2.imshow("Card Detection", img)
+    
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-cap.release()
+#cap.release()
 cv2.destroyAllWindows()
